714be621e63e91f735742c59815b6566468d4b97
misaki <mwtj96762@gmail.com>
2025-08-27 22:22:09 +0900

    データベース接続設定を行う

---
+++ backend/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+#.envファイルから環境変数を読み込む
+load_dotenv()
+
+#データベースURLを取得
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+#SQLAlchemyのエンジンを作成
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+#セッションクラスを作成
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
