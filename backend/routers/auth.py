9c2333c36fba338ce2a4f89659ecda905b7cfd74
misaki <mwtj96762@gmail.com>
2025-10-07 23:04:51 +0900

    feature:ユーザー作成エンドポイントを追加（パスワードハッシュ化対応）

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,6 +1,19 @@
 from fastapi import APIRouter
+from ..models.models import Users
+from ..schemas.schemas import UserRequest
+from passlib.context import CryptContext
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 router = APIRouter(
     prefix="/auth",
     tags=["auth"]
 )
+
+@router.post("/auth")
+def create_user(userRequest: UserRequest):
+  user = Users(
+    user_name=userRequest.user_name,
+    email=userRequest.email,
+    hashed_password=userRequest.bcrypt_context.hash(userRequest.password)
+  )
