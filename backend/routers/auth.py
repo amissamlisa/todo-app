ec16805ca485cb2a82e3b5301c3ae281274856f6
misaki <mwtj96762@gmail.com>
2025-10-14 00:12:08 +0900

    JWT認証の関数を作成

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,19 +1,77 @@
-from fastapi import APIRouter
+from alembic.util import status
+from fastapi import APIRouter, Depends, HTTPException
+from datetime import timedelta, datetime, timezone
 from ..models.models import Users
+from ..repository.repository import UserRepository
 from ..schemas.schemas import UserRequest
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from typing import Annotated
+from ..database import get_db
+from jose import jwt, JWTError
+from dotenv import load_dotenv
+import os
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 router = APIRouter(
     prefix="/auth",
     tags=["auth"]
 )
 
-@router.post("/auth")
-def create_user(userRequest: UserRequest):
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = 'HS256'
+
+def authenticate_user(username: str, password: str, db: Session = Depends(get_db)):
+  user = db.query(Users).filter(Users.username == username).first()
+  if not user:
+    return False
+  if not bcrypt_context.verify(password, user.hashed_password):
+    return False
+  return True
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+  encode = {'sub': username, 'name': user_id}
+  expires = datetime.now(timezone.utc) + expires_delta
+  encode.update({'exp': expires})
+  return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+  try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    username: str = payload.get('sub')
+    user_id: int = payload.get('id')
+    if username is None or user_id is None:
+      raise HTTPException(status_code=401, detail='ユーザーを検証できません')
+
+  except JWTError:
+    raise HTTPException(status_code=401, detail='ユーザーを検証できません')
+
+  return {'username': username, 'user_id': user_id}
+
+@router.post("/")
+def create_user(userRequest: UserRequest,db: Session = Depends(get_db)):
+  user_repository = UserRepository()
   user = Users(
     user_name=userRequest.user_name,
     email=userRequest.email,
     hashed_password=userRequest.bcrypt_context.hash(userRequest.password)
   )
+
+  user_repository.registerUsers(user)
+
+
+
+@router.post("/token")
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],db: Session = Depends(get_db)):
+  user = authenticate_user(form_data.username, form_data.password, db)
+
+  if not user:
+    raise HTTPException(status_code=401, detail='ユーザーを検証できません')
+  token = create_access_token(user.username, user.user_id, timedelta(minutes=15))
+  return token
+  
