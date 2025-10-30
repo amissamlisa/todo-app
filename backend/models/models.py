90f5cda60b8f15c53d50e818283f21966404fd4a
misaki <mwtj96762@gmail.com>
2025-10-30 19:59:19 +0900

    それぞれのモデルクラスにCheck制約とバリデーション、必要なメソッドを追加。DailyTasksモデルクラスを削除

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import String, ForeignKey, Date, DateTime, Enum, Numeric
+from sqlalchemy import String, ForeignKey, Date, DateTime, Enum, Numeric, CheckConstraint
 import enum
 from sqlalchemy.sql import func
-from decimal import Decimal 
-from sqlalchemy.orm import DeclarativeBase
+from decimal import Decimal
+from sqlalchemy.orm import DeclarativeBase, validates
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Mapped
 import datetime
@@ -10,80 +10,147 @@ import jpholiday
 
 PRECISION, SCALE = 4, 1
 
+
 class GoalsTasksStatusEnum(enum.Enum):
-  Todo = "未着手"
-  InProgress = "作業中"
-  Completed = "完了"
+    Todo = "未着手"
+    InProgress = "作業中"
+    Completed = "完了"
+
 
 class GoalsStatusEnum(enum.Enum):
-  Unachieved = "未達成"
-  Achieved = "達成"
+    Unachieved = "未達成"
+    Achieved = "達成"
+
 
 class Base(DeclarativeBase):
     pass
 
+
 class Goals(Base):
-  __tablename__ = "goals"
+    __tablename__ = "goals"
 
-  goal_id: Mapped[int] = mapped_column(primary_key=True)
-  goal_name: Mapped[str] = mapped_column(String(100), nullable=False)
-  status: Mapped[GoalsStatusEnum] = mapped_column(Enum(GoalsStatusEnum), default=GoalsStatusEnum.Unachieved, nullable=False)
-  start_day: Mapped[Date] = mapped_column(Date, nullable=False)
-  target_day: Mapped[Date] = mapped_column(Date, nullable=False)
-  status_against_goal: Mapped[str] = mapped_column(String(200), nullable=False)
-  weekday_available_hours: Mapped[Decimal] = mapped_column(Numeric(PRECISION, SCALE), nullable=False)
-  weekends_available_hours: Mapped[Decimal] = mapped_column(Numeric(PRECISION, SCALE), nullable=False)
-  total_estimated_time: Mapped[Decimal] = mapped_column(Numeric(PRECISION, SCALE), nullable=False)
-  task_creation_rule: Mapped[str | None] = mapped_column(String(800))
-  created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.current_timestamp(), nullable=False)
+    goal_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"), nullable=False)
+    goal_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    status: Mapped[GoalsStatusEnum] = mapped_column(Enum(GoalsStatusEnum), default=GoalsStatusEnum.Unachieved,
+                                                    nullable=False)
+    start_day: Mapped[Date] = mapped_column(Date, nullable=False)
+    target_day: Mapped[Date] = mapped_column(Date, nullable=False)
+    status_against_goal: Mapped[str] = mapped_column(String(200), nullable=False)
+    weekday_available_hours: Mapped[Decimal] = mapped_column(Numeric(PRECISION, SCALE), nullable=False)
+    weekends_available_hours: Mapped[Decimal] = mapped_column(Numeric(PRECISION, SCALE), nullable=False)
+    total_estimated_time: Mapped[Decimal] = mapped_column(Numeric(PRECISION, SCALE), nullable=False)
+    task_creation_rule: Mapped[str | None] = mapped_column(String(800))
+    created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.current_timestamp(), nullable=False)
+    __table_args__ = (
+        CheckConstraint("length(goal_name) > 0", name="check_goal_name_greater_than_zero"),
+        CheckConstraint('start_day <= target_day', name='check_start_before_target'),
+        CheckConstraint('start_day >= CURRENT_DATE', name='check_start_before_now'),
+        CheckConstraint('target_day >= CURRENT_DATE', name='check_target_before_now'),
+        CheckConstraint('length(status_against_goal) > 0', name="check_status_against_goal_greater_than_zero"),
+        CheckConstraint('weekday_available_hours > 0', name="check_weekday_available_hours_greater_than_zero"),
+        CheckConstraint('weekends_available_hours > 0', name="check_weekends_available_hours_greater_than_zero"),
+        CheckConstraint('total_estimated_time > 0', name="check_total_estimated_time_greater_than_zero"),
+    )
 
+    @validates('goal_name', 'status_against_goal')
+    def validate_goal_name(self, key, value: str) -> str:
+        if value is None:
+            raise ValueError("値がNoneです")
+        cleaned = value.strip().replace('\u3000', '')
+        return cleaned
 
-  def calculate_total_estimated_time(self):
-    weekday_count = Decimal(0)
-    weekends_count = Decimal(0)
-    holiday_count = Decimal(0)
+    @validates('weekday_available_hours', 'weekends_available_hours', 'total_estimated_time')
+    def validate_hour_digits(self, key, value):
+        if value.as_tuple().exponent < -1:
+            raise ValueError("小数点の桁数が1桁より大きいです")
+        return value
 
-    current_day: datetime.date = self.start_day
-    while current_day <= self.target_day:
-      if jpholiday.is_holiday(current_day):
-        holiday_count += self.weekends_available_hours
-      elif current_day.weekday() < 5:
-        weekday_count += self.weekday_available_hours
-      else:
-        weekends_count += self.weekends_available_hours
-      current_day += datetime.timedelta(days=1)
-    total_estimated_time =  weekends_count + weekday_count + holiday_count
+    @validates('start_day', 'target_day')
+    def validate_date(self, key, value):
+        if not isinstance(value, datetime.date):
+            raise TypeError("日付はDate型でなければいけません")
+        return value
 
-    return total_estimated_time
+    def calculate_total_estimated_time(self):
+        weekday_count = Decimal(0)
+        weekends_count = Decimal(0)
+        holiday_count = Decimal(0)
+
+        current_day: datetime.date = self.start_day
+        while current_day <= self.target_day:
+            if jpholiday.is_holiday(current_day):
+                holiday_count += self.weekends_available_hours
+            elif current_day.weekday() < 5:
+                weekday_count += self.weekday_available_hours
+            else:
+                weekends_count += self.weekends_available_hours
+            current_day += datetime.timedelta(days=1)
+        total_estimated_time = weekends_count + weekday_count + holiday_count
+
+        return total_estimated_time
+
 
 class GoalsTasks(Base):
-  __tablename__ = "goals_tasks"
+    __tablename__ = "goals_tasks"
 
-  goal_task_id: Mapped[int] = mapped_column(primary_key=True)
-  goal_id: Mapped[int] = mapped_column(ForeignKey("goals.goal_id"), nullable=False)
-  goal_task_name: Mapped[str] = mapped_column(String(50), nullable=False)
-  goal_task_status: Mapped[GoalsTasksStatusEnum] = mapped_column(Enum(GoalsTasksStatusEnum), default=GoalsTasksStatusEnum.Todo, nullable=False)
-  deadline: Mapped[Date] = mapped_column(Date, nullable=False)
-  estimated_time: Mapped[Decimal] = mapped_column(Numeric(PRECISION, SCALE), nullable=False)
-  created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.current_timestamp(), nullable=False)
+    goal_task_id: Mapped[int] = mapped_column(primary_key=True)
+    goal_id: Mapped[int] = mapped_column(ForeignKey("goals.goal_id"), nullable=False)
+    goal_task_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    goal_task_status: Mapped[GoalsTasksStatusEnum] = mapped_column(Enum(GoalsTasksStatusEnum),
+                                                                   default=GoalsTasksStatusEnum.Todo, nullable=False)
+    deadline: Mapped[Date] = mapped_column(Date, nullable=False)
+    estimated_time: Mapped[Decimal] = mapped_column(Numeric(PRECISION, SCALE), nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.current_timestamp(), nullable=False)
+    __table_args__ = (
+        CheckConstraint("length(goal_task_name) > 0", name="check_goal_task_name_greater_than_zero"),
+        CheckConstraint('deadline >= CURRENT_DATE', name='check_deadline_before_now'),
+        CheckConstraint('estimated_time > 0', name="check_estimated_time_greater_than_zero"),
+    )
 
-class DailyTasks(Base):
-  __tablename__ = "daily_tasks"
-  daily_task_id: Mapped[int] = mapped_column(primary_key=True)
-  user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"), nullable=False)
-  daily_task_name: Mapped[str] = mapped_column(String(50), nullable=False)
-  goal_task_status: Mapped[GoalsTasksStatusEnum] = mapped_column(Enum(GoalsTasksStatusEnum), default=GoalsTasksStatusEnum.Todo, nullable=False)
-  deadline: Mapped[Date] = mapped_column(Date, nullable=False)
-  estimated_time: Mapped[Decimal] = mapped_column(Numeric(PRECISION, SCALE), nullable=False)
-  created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.current_timestamp(), nullable=False)
+    @validates('goal_task_name')
+    def validate_goal_name(self, key, value: str) -> str:
+        if value is None:
+            raise ValueError("値がNoneです")
+        cleaned = value.strip().replace('\u3000', '')
+        return cleaned
+
+    @validates('deadline')
+    def validate_date(self, key, value):
+        if not isinstance(value, datetime.date):
+            raise TypeError("期限はDate型でなければいけません")
+        return value
+
+    @validates('estimated_time')
+    def validate_hour_digits(self, key, value):
+        if value.as_tuple().exponent < -1:
+            raise ValueError("小数点の桁数が1桁より大きいです")
+        return value
+
 
 class Users(Base):
-  __tablename__ = "users"
-  user_id: Mapped[int] = mapped_column(primary_key=True)
-  goal_id: Mapped[int] = mapped_column(ForeignKey("goals.goal_id"), nullable=False)
-  daily_task_id: Mapped[int] = mapped_column(ForeignKey("daily_tasks.daily_task_id"))
-  username: Mapped[str] = mapped_column(String(50), nullable=False)
-  hashed_password: Mapped[str] = mapped_column(unique=True, nullable=False)
-  email: Mapped[str] = mapped_column(unique=True, nullable=False)
-  user_points: Mapped[int] = mapped_column(nullable=True)
-  created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.current_timestamp(), nullable=False)
+    __tablename__ = "users"
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    user_points: Mapped[int] = mapped_column(default=0, nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.current_timestamp(), nullable=False)
+    __table_args__ = (
+        CheckConstraint("length(username) > 2", name="check_username_greater_than_two"),
+        CheckConstraint("user_points >= 0", name="check_username_more_than_zero"),
+    )
+
+    @validates('username')
+    def validate_goal_name(self, key, value: str) -> str:
+        if value is None:
+            raise ValueError("値がNoneです")
+        cleaned = value.strip().replace('\u3000', '')
+        return cleaned
+
+    @validates('user_points')
+    def validate_hour_digits(self, key, value):
+        decimal_value = Decimal(str(value))
+        if decimal_value.as_tuple().exponent <= -1:
+            raise ValueError("小数点の値は無効です")
+        return value
