66e687b0f9eedc66db7e522489575fb82b5031f5
misaki <mwtj96762@gmail.com>
2025-11-02 14:41:46 +0900

    テスト用のデータベースURLに変更

--- backend/database.py
+++ backend/database.py
@@ -2,13 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
-
+from urllib.parse import quote_plus
 # .envファイルから環境変数を読み込む
 load_dotenv()
 
 # 本番用データベースURLを取得
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_PRODUCTION_DATABASE_URL")
+# SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_PRODUCTION_DATABASE_URL")
 
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 if SQLALCHEMY_DATABASE_URL is None:
     raise RuntimeError( "Environment variable 'SQLALCHEMY_DATABASE_URL' is not set.\n"
         "Please set this variable in your environment or create a .env file in the project root with a line like:\n"
