79fe7aec5fc98c4c50d778e39e8b6c253e003e47
misaki <mwtj96762@gmail.com>
2025-11-05 21:03:53 +0900

    fix:goal_task_statusとgoal_statusの値を変更するための関数・goalの値を登録する関数を変更

--- backend/repository/repository.py
+++ backend/repository/repository.py
@@ -1,5 +1,3 @@
-import datetime
-
 from sqlalchemy import update
 from sqlalchemy.orm import Session
 from ..models.models import Goals, GoalsStatusEnum, GoalsTasks, GoalsTasksStatusEnum, Users
@@ -11,11 +9,15 @@ class GoalRepository:
 
     def register_goal(self, db, goal: Goals, commit=True):
         try:
-            db.add(goal)
-            db.flush()
+            existing_goal = db.query(Goals).filter(Goals.status == GoalsStatusEnum.Unachieved.value).first()
+            if existing_goal:
+                raise ValueError("未達成のゴールが存在します")
             if commit:
+                db.add(goal)
+                db.flush()
                 db.commit()
-            return goal
+                db.refresh(goal)
+                return goal
         except Exception as e:
             db.rollback()
             raise e
@@ -29,16 +31,21 @@ class GoalRepository:
             db.rollback()
             raise e
 
-    def update_goal_status_from_db(self, db: Session, goal_id, commit=True):
+    def update_goal_status_from_db(self, db: Session, goal_id, new_goal_status, commit=True):
         try:
-            stmt = (
-                update(Goals)
-                .where(Goals.status == GoalsStatusEnum.Unachieved, Goals.goal_id == goal_id)
-                .values(status=GoalsStatusEnum.Achieved)
-            )
-            db.execute(stmt)
-            if commit:
-                db.commit()
+            goal = db.query(Goals).filter(Goals.goal_id == goal_id).first()
+            if goal is None:
+                raise ValueError("目標が見つかりません")
+            # ステータス更新
+            if goal.status != new_goal_status.value:
+                goal.status = new_goal_status.value
+                if commit:
+                    db.commit()
+                    db.refresh(goal)
+                return goal
+            else:
+                raise ValueError("同じステータスの変更はできません")
+
         except Exception as e:
             db.rollback()
             raise e
@@ -68,23 +75,20 @@ class GoalTaskRepository:
             db.rollback()
             raise e
 
-    def update_goal_task_stauts_from_db(self, db: Session, goal_task_id, new_goal_task_status: GoalsTasksStatusEnum,
+    def update_goal_task_status_from_db(self, db: Session, goal_task_id, new_goal_task_status: GoalsTasksStatusEnum,
                                         commit=True):
         try:
-            curr_goal_task_status = (
-                db.query(GoalsTasks.goal_task_status)
-                .filter(GoalsTasks.goal_task_id == goal_task_id)
-                .scalar()
-            )
-            if curr_goal_task_status != new_goal_task_status:
-                stmt = (
-                    update(GoalsTasks)
-                    .where(GoalsTasks.goal_task_id == goal_task_id)
-                    .values(goal_task_status=new_goal_task_status)
-                )
-                db.execute(stmt)
+            goal_task = db.query(GoalsTasks).filter(GoalsTasks.goal_task_id == goal_task_id).first()
+            if goal_task is None:
+                raise ValueError("目標が見つかりません")
+
+            # ステータス更新
+            if goal_task.goal_task_status != new_goal_task_status.value:
+                goal_task.goal_task_status = new_goal_task_status.value
                 if commit:
                     db.commit()
+                    db.refresh(goal_task)
+                return goal_task
             else:
                 raise ValueError("同じステータスの変更はできません")
         except Exception as e:
