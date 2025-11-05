be798baaf1e04e972c6e09e725eb733c2f0366cb
misaki <mwtj96762@gmail.com>
2025-11-05 23:03:29 +0900

    fix:weekday_available_hoursをweekday_available_time、weekends_available_hoursをweekends_available_timeに変更し、GoalsTasksのgoal_task_statusにバリデーション追加し、estimated_timeの値上限のCheck制約を追加、Goalsクラスのcalculate_total_estimated_timeメソッドの条件分岐を修正

--- backend/models/models.py
+++ backend/models/models.py
@@ -32,8 +32,8 @@ class Goals(Base):
     start_day: Mapped[Date] = mapped_column(Date, nullable=False)
     target_day: Mapped[Date] = mapped_column(Date, nullable=False)
     status_against_goal: Mapped[str] = mapped_column(String(200), nullable=False)
-    weekday_available_hours:  Mapped[int] = mapped_column(Integer, nullable=False)
-    weekends_available_hours:  Mapped[int] = mapped_column(Integer, nullable=False)
+    weekday_available_time:  Mapped[int] = mapped_column(Integer, nullable=False)
+    weekends_available_time:  Mapped[int] = mapped_column(Integer, nullable=False)
     total_estimated_time:  Mapped[int] = mapped_column(Integer, nullable=False)
     task_creation_rule: Mapped[str | None] = mapped_column(String(800))
     created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.current_timestamp(), nullable=False)
@@ -43,8 +43,8 @@ class Goals(Base):
         CheckConstraint('start_day > CURRENT_DATE', name='check_start_before_now'),
         CheckConstraint('target_day > CURRENT_DATE', name='check_target_before_now'),
         CheckConstraint('length(status_against_goal) > 0', name="check_status_against_goal_greater_than_zero"),
-        CheckConstraint('weekday_available_hours > 0', name="check_weekday_available_hours_greater_than_zero"),
-        CheckConstraint('weekends_available_hours > 0', name="check_weekends_available_hours_greater_than_zero"),
+        CheckConstraint('weekday_available_time > 0', name="check_weekday_available_time_greater_than_zero"),
+        CheckConstraint('weekends_available_time > 0', name="check_weekends_available_time_greater_than_zero"),
         CheckConstraint('total_estimated_time > 0', name="check_total_estimated_time_greater_than_zero"),
     )
 
@@ -54,6 +54,8 @@ class Goals(Base):
             raise ValueError("値がNoneです")
         cleaned = value.strip().replace('\u3000', '')
         return cleaned
+
+
 
     @validates('start_day', 'target_day')
     def validate_date(self, key, value):
@@ -67,13 +69,13 @@ class Goals(Base):
         holiday_count = 0
 
         current_day: datetime.date = self.start_day
-        while current_day <= self.target_day:
+        while current_day <= self.target_day and self.start_day != self.target_day:
             if jpholiday.is_holiday(current_day):
-                holiday_count += self.weekends_available_hours
+                holiday_count += self.weekends_available_time
             elif current_day.weekday() < 5:
-                weekday_count += self.weekday_available_hours
+                weekday_count += self.weekday_available_time
             else:
-                weekends_count += self.weekends_available_hours
+                weekends_count += self.weekends_available_time
             current_day += datetime.timedelta(days=1)
         total_estimated_time = weekends_count + weekday_count + holiday_count
 
@@ -95,6 +97,7 @@ class GoalsTasks(Base):
         CheckConstraint("length(goal_task_name) > 0", name="check_goal_task_name_greater_than_zero"),
         CheckConstraint('deadline > CURRENT_DATE', name='check_deadline_before_now'),
         CheckConstraint('estimated_time > 0', name="check_estimated_time_greater_than_zero"),
+        CheckConstraint('estimated_time < 1441', name="check_estimated_time_greater_than_1440"),
     )
 
     @validates('goal_task_name')
@@ -103,6 +106,12 @@ class GoalsTasks(Base):
             raise ValueError("値がNoneです")
         cleaned = value.strip().replace('\u3000', '')
         return cleaned
+
+    @validates('goal_task_status')
+    def validate_goal_task_status(self, key, value: str) -> str:
+        if not value in GoalsTasksStatusEnum:
+            raise ValueError("無効な値です")
+        return value
 
     @validates('deadline')
     def validate_date(self, key, value):
