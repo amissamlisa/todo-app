35ef129421e1b6ad4471c31cf33ab6c3ef46dd93
misaki <mwtj96762@gmail.com>
2025-11-08 18:15:37 +0900

    fix:APIレスポンスの形式修正・それに伴い値の取得方法を変更・複数の例外をまとめてキャッチし、内容を detail に入れて HTTP 400で返す処理をgenerate_chat_replyメソッドに追加

--- backend/routers/goal_tasks.py
+++ backend/routers/goal_tasks.py
@@ -1,10 +1,12 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.exc import IntegrityError, StatementError, DataError
+
 from ..database import get_db
 from sqlalchemy.orm import Session
-from ..schemas.schemas import GoalsTasksRequest, GoalsTasksOut
-from ..models.models import Goals, GoalsTasks, GoalsTasksStatusEnum
+from ..schemas.schemas import GoalsTasksOut, GoalsRequest
+from ..models.models import GoalsTasks, GoalsTasksStatusEnum, Goals
 from openai import OpenAI
 import os
 import json
@@ -23,13 +25,13 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 @router.get("/{goal_task_id}", status_code=status.HTTP_200_OK)
 def read_goal_tasks(user: user_dependency, db: db_dependency, goal_task_id: int):
     if user is None:
-        raise HTTPException(status_code=404, detail="認証に失敗")
+        raise HTTPException(status_code=404, detail="認証に失敗しました")
     goal_tasks = db.query(GoalsTasks).filter(GoalsTasks.goal_task_id == goal_task_id).all()
 
-    if goal_tasks is not None:
-        return goal_tasks
+    if goal_tasks is None:
+        raise HTTPException(status_code=404, detail='目標達成タスクが見つかりません')
 
-    raise HTTPException(status_code=404, detail='目標達成タスクが見つかりません')
+    return {"detail": "目標達成タスクを取得しました", "goal_tasks": goal_tasks}
 
 
 @router.delete("/{goal_task_id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -44,32 +46,33 @@ def delete_goal_tasks(user: user_dependency, db: db_dependency, goal_task_id: in
 
     goal_task_repository.delete_goal_task_from_db(db, goal_task, commit=True)
 
+    return {"detail": "目標達成タスクを削除しました"}
+
 
 @router.put("/{goal_task_id}", status_code=status.HTTP_204_NO_CONTENT)
 def update_goal_task_status(user: user_dependency, db: db_dependency, goal_task_id: int,
                             new_status: GoalsTasksStatusEnum):
-    goal_task_repository = GoalTaskRepository()
     if user is None:
-        raise HTTPException(status_code=404, detail="認証に失敗")
+        raise HTTPException(status_code=404, detail="認証に失敗しました")
 
     # リポジトリ呼び出し
     goal_task_repository = GoalTaskRepository()
     updated_task = goal_task_repository.update_goal_task_status_from_db(
         db, goal_task_id, new_status, commit=True
     )
-    # print(updated_task.goal_task_status)
 
     if updated_task is None:
         raise HTTPException(status_code=404, detail="目標達成タスクが見つかりません")
 
-    return {"message": "ステータスを更新しました"}
+    return {"detail": "目標達成タスクのステータスを更新しました"}
 
 
-# stringstri123@A
-
-
-def generate_chat_reply(goal: GoalsTasksRequest):
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def generate_chat_reply(goal: GoalsRequest, user: user_dependency):
     try:
+        if user is None:
+            raise HTTPException(status_code=404, detail="userが見つかりません")
+
         client = OpenAI(
             # This is the default and can be omitted
             api_key=os.environ.get("OPENAI_API_KEY"),
@@ -112,12 +115,12 @@ def generate_chat_reply(goal: GoalsTasksRequest):
               "goal_task_name": "基本情報技術者試験の1問1答をする",
               "deadline": "2025-09-11",
               "estimated_time": 35
-            }}
+            }},
             {{
               "goal_task_name": "基本情報技術者試験の過去問を解く",
               "deadline": "2025-09-12",
               "estimated_time": 90
-            }}
+            }},
           ]
         }}
             注意事項
@@ -144,45 +147,51 @@ def generate_chat_reply(goal: GoalsTasksRequest):
         for task in tasks_json["goal_tasks"]:
             goal_tasks.append(GoalsTasksOut(**task))
 
-        return goal_tasks
-
+        return {"detail": "目標達成タスクを生成しました", "goal_tasks": goal_tasks, "goal": goal}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except IntegrityError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except StatementError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except DataError as e:
+        raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def save_goal_and_goals_tasks(user: user_dependency, goal_tasks_request: GoalsTasksRequest,
-                              db: Session = Depends(get_db)):
-    if user is None:
-        raise HTTPException(status_code=404, detail="goalが見つかりません")
+def save_goals_and_goal_tasks_and(user: user_dependency, json_string,
+                                  db: Session = Depends(get_db)):
     try:
+        if user is None:
+            raise HTTPException(status_code=404, detail="認証に失敗しました")
         goal_repository = GoalRepository()
-        goal_task_repository = GoalTaskRepository()
-        goal = Goals(goal_name=goal_tasks_request.goal_name,
-                     status_against_goal=goal_tasks_request.status_against_goal,
-                     start_day=goal_tasks_request.start_day,
-                     target_day=goal_tasks_request.target_day,
-                     weekday_available_hours=goal_tasks_request.weekday_available_hours,
-                     weekends_available_hours=goal_tasks_request.weekends_available_hours,
-                     task_creation_rule=goal_tasks_request.task_creation_rule,
-                     user_id=user.get('user_id'))
+        data = json.loads(json_string)
+        goal = data["goal"]
+        goal_task_list = goal["goal_tasks"]
 
-        goal_task_list_value = generate_chat_reply(goal)
         goal.total_estimated_time = goal.calculate_total_estimated_time()
-        goal_data = goal_repository.register_goal(db, goal)
+        goal = Goals(**goal.model_dump(), user_id=user.get("id"))
+        goal_repository.register_goal(db, goal, commit=True)
 
-        for goal_task in goal_task_list_value:
+        goal_task_repository = GoalTaskRepository()
+        for goal_task in goal_task_list:
             goal_task_item = GoalsTasks(goal_task_name=goal_task.goal_task_name, deadline=goal_task.deadline,
                                         estimated_time=goal_task.estimated_time,
-                                        goal_id=goal_data.goal_id)
-            try:
-                goal_task_repository.register_goal_task(db, goal_task_item)
-            except Exception as e:
-                raise HTTPException(status_code=500, detail=str(e, "データが登録されません"))
+                                        goal_id=goal.goal_id)
+            goal_task_repository.register_goal_task(db, goal_task_item)
         return {
-            "status": "ok",
-            "message": "達成目標と目標達成タスクが保存されました",
-            "goal_id": goal_data.goal_id
+            "detail": "達成目標と目標達成タスクが保存されました",
+            "goal_id": goal.goal_id
         }
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except IntegrityError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except StatementError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except DataError as e:
+        raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
