ecb6efc5b870e6e33b1c7ac532d80616c1651d61
misaki <mwtj96762@gmail.com>
2025-11-09 19:30:24 +0900

    不要なエンドポイント削除

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,6 @@ from .routers import goal_tasks, auth, goal
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"hello": "world"}
-
 app.include_router(goal_tasks.router)
 app.include_router(auth.router)
 app.include_router(goal.router)
