27a268b655203b17f439fdbc41f25e1050dc3ff3
misaki <mwtj96762@gmail.com>
2025-12-19 01:23:22 +0900

    feature:TwoButtonに背景色と文字色のpropsを追加

--- frontend/src/shared/components/molecules/TwoButton.tsx
+++ frontend/src/shared/components/molecules/TwoButton.tsx
@@ -2,13 +2,13 @@ import { memo } from "react";
 import { Button } from "../atoms/Button";
 import type { TwoButtonProps } from "../../types/twoButton";
 
-export const TwoButton = memo(({ buttonTitle1, buttonTitle2 }: TwoButtonProps) => {
+export const TwoButton = memo(({ buttonTitle1, buttonTitle2, buttonBgColor, buttonTextColor }: TwoButtonProps) => {
   return (
     <div >
       <div className="mb-[3vh]">
-        <Button buttonColor="bg-secondary" textColor="text-primary">{buttonTitle1}</Button>
+        <Button buttonColor={buttonBgColor} textColor={buttonTextColor}>{buttonTitle1}</Button>
       </div>
-      <Button buttonColor="bg-secondary" textColor="text-primary">{buttonTitle2}</Button>
+      <Button buttonColor={buttonBgColor} textColor={buttonTextColor}>{buttonTitle2}</Button>
     </div>
   )
 })
