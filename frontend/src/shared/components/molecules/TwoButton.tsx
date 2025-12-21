95b8c2940c89f934bbf4a7ff506cc47880ef494d
misaki <mwtj96762@gmail.com>
2025-12-21 17:24:09 +0900

    feature:TwoButton にクリックイベント用メソッドを追加

--- frontend/src/shared/components/molecules/TwoButton.tsx
+++ frontend/src/shared/components/molecules/TwoButton.tsx
@@ -2,13 +2,13 @@ import { memo } from "react";
 import { Button } from "../atoms/Button";
 import type { TwoButtonProps } from "../../types/twoButton";
 
-export const TwoButton = memo(({ buttonTitle1, buttonTitle2, buttonBgColor, buttonTextColor }: TwoButtonProps) => {
+export const TwoButton = memo(({ buttonTitle1, buttonTitle2, buttonBgColor, buttonTextColor,  onPrimaryClick, onSecondaryClick}: TwoButtonProps) => {
   return (
     <div >
       <div className="mb-[3vh]">
-        <Button buttonColor={buttonBgColor} textColor={buttonTextColor}>{buttonTitle1}</Button>
+        <Button onButtonClick={onPrimaryClick}  buttonColor={buttonBgColor} textColor={buttonTextColor}  >{buttonTitle1}</Button>
       </div>
-      <Button buttonColor={buttonBgColor} textColor={buttonTextColor}>{buttonTitle2}</Button>
+      <Button onButtonClick={onSecondaryClick}  buttonColor={buttonBgColor} textColor={buttonTextColor} >{buttonTitle2}</Button>
     </div>
   )
 })
--- frontend/src/shared/types/twoButton.ts
+++ frontend/src/shared/types/twoButton.ts
@@ -1,8 +1,11 @@
 type ButtonBgColor = "bg-primary" | "bg-secondary";
 type ButtonTextColor = "text-primary" | "text-secondary";
+
 export interface TwoButtonProps {
   buttonTitle1: string
   buttonTitle2: string
   buttonBgColor: ButtonBgColor
   buttonTextColor: ButtonTextColor
+  onPrimaryClick: () => void;
+  onSecondaryClick: () => void;
 }
