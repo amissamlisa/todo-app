d84c6aed1a5ec14a1d68efb6efedf46363c1e750
misaki <mwtj96762@gmail.com>
2025-12-21 17:32:58 +0900

    feature:PasswordInputにonChange処理を追加しformNameと値を親へ渡すように修正

--- frontend/src/shared/components/molecules/PasswordInput.tsx
+++ frontend/src/shared/components/molecules/PasswordInput.tsx
@@ -4,7 +4,11 @@ import { IoIosEye } from "react-icons/io";
 import { IoIosEyeOff } from "react-icons/io";
 import type { InputProps } from "../../types/input";
 
-export const PasswordInput = memo(({ textColor, borderColor, children, formName}: InputProps) => {
+export const PasswordInput = memo(({ textColor, borderColor, children, formName, handleValueChange}: InputProps) => {
+   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    handleValueChange(formName, inputValue);
+  }
   const [eye, setEye] = useState(false);
   const [formType, setFormType] = useState("password");
   const handleEyeClick = (eye: boolean) => {
@@ -14,7 +18,7 @@ export const PasswordInput = memo(({ textColor, borderColor, children, formName}
   return (
     <div className="flex relative flex-col w-[clamp(93px,68vw,400px)]">
       <label className={`${textColor} font-bold`} htmlFor={`${formName}`}>{children}</label>
-      <input className={`${borderColor} focus:outline-none 
+      <input onChange={handleInputChange} className={`${borderColor} focus:outline-none 
  rounded-[5px] w-full p-2  border-4 border-solid block bg-secondary`} type={`${formType}`} name={`${formName}`} />
       {eye === true && <IoIosEye onClick={() => handleEyeClick(eye)} className="text-primary absolute right-3 bottom-2/12" size={24} />}
       {eye === false && <IoIosEyeOff onClick={() => handleEyeClick(eye)}   className="text-primary absolute right-3 bottom-2/12" size={24}/>}
