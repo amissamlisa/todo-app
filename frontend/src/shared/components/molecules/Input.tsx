a1f25b179cfc97bc03424ea386c18d703cdcbfb1
misaki <mwtj96762@gmail.com>
2025-12-21 17:29:08 +0900

    feature:Inputコンポーネントに値変更ハンドラを追加とprops記述を整理

--- frontend/src/shared/components/molecules/Input.tsx
+++ frontend/src/shared/components/molecules/Input.tsx
@@ -1,12 +1,16 @@
 import { memo } from "react";
 import type { InputProps } from "../../types/input";
 
-export const Input = memo(({ textColor, borderColor, children, formName, formType }: InputProps) => {
+export const Input = memo(({ textColor, borderColor, children, formName, formType, handleValueChange }: InputProps) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    handleValueChange(formName,inputValue);
+  }
   return (
     <div className="flex flex-col w-[clamp(93px,68vw,400px)]">
-      <label className={`${textColor} font-bold`}  htmlFor={`${formName}`}>{children}</label>
+      <label className={`${textColor} font-bold`} htmlFor={`${formName}`}>{children}</label>
       <input className={`${borderColor} focus:outline-none 
- rounded-[5px] w-full p-2  border-4 border-solid block bg-secondary`} type={`${formType}`} name={`${formName}`} />
+ rounded-[5px] w-full p-2  border-4 border-solid block bg-secondary`} type={formType} id={formName} name={formName} onChange={handleInputChange} />
     </div>
   )
 })
--- frontend/src/shared/types/input.ts
+++ frontend/src/shared/types/input.ts
@@ -1,10 +1,14 @@
 import { type ReactNode } from "react";
 type FormTextColor = "text-primary" | "text-secondary";
 type FormBorderColor = "border-primary" | "border-secondary";
+type HandleValueChange = (formName: string, newValue: string) => void;
+type InputType = "text" | "password";
+
 export interface InputProps {
   borderColor: FormBorderColor
   textColor: FormTextColor
-  formName: string,
-  formType?: string,
+  formName: string
+  formType?: InputType
+  handleValueChange: HandleValueChange
   children: ReactNode
 }
