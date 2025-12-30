56e64f80614ad2e132be0bd8c77534d1092de0bb
misaki <mwtj96762@gmail.com>
2025-12-30 18:01:11 +0900

    refactor: #29 新規会員登録画面のフォーム管理を react-hook-form に統一

--- frontend/src/features/users/pages/RegistrationForm.tsx
+++ frontend/src/features/users/pages/RegistrationForm.tsx
@@ -1,30 +1,34 @@
-import { memo, useState } from "react";
+import { memo } from "react";
 import { useNavigate } from 'react-router-dom';
+import { Controller, useForm } from "react-hook-form"
 import { Header } from "../../../shared/components/molecules/Header";
 import { Input } from "../../../shared/components/molecules/Input";
-import { PasswordInput } from "../../../shared/components/molecules/PasswordInput";
 import { TwoButton } from "../../../shared/components/molecules/TwoButton";
+import { PasswordInput } from "../../../shared/components/molecules/PasswordInput";
 
 
 export const RegistrationForm = memo(() => {
   const navigate = useNavigate();
-  const [formValues, setFormValues] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: ""
+
+  type RegistrationFormType =
+    {
+      username: string,
+      email: string,
+      password: string,
+      confirmPassword: string
+    }
+  const { control, handleSubmit, formState: { errors }, getValues} = useForm<RegistrationFormType>({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    }
   });
-  const handleValueChange =
-    (formName: string, newValue: string) => {
-      setFormValues((prev) => ({
-        ...prev,
-        [formName]: newValue,
-      }));
-      console.log(formValues);
-    };
-  const onPrimaryClick = () => {
-    navigate("/user-registration/confirm", {state: formValues});
-  }
+  const onPrimaryClick = (data: RegistrationFormType) => {
+    console.log(data);
+    navigate("/user-registration/confirm", { state: data });
+  };
 
   const onSecondaryClick = () => {
     navigate("/");
@@ -35,19 +39,118 @@ export const RegistrationForm = memo(() => {
       <div className="bg-secondary flex flex-col items-center">
         <h2 className="text-primary mt-[clamp(15px,4vh,60px)] mb-[clamp(20px,4.8vh,80px)] text-2xl">新規会員登録</h2>
         <div className="mb-[clamp(10px,2.6vh,40px)]">
-          <Input handleValueChange={handleValueChange} textColor="text-primary" borderColor="border-primary" formType="text" formName="username">ユーザー名</Input>
+          <Controller
+            control={control}
+            rules={{
+              required: "ユーザー名を入力してください",
+            }}
+            name="username"
+            render={({ field }) => (
+              <Input
+                value={field.value}
+                onChangeText={field.onChange}
+                onBlur={field.onBlur}
+                textColor="text-primary"
+                borderColor="border-primary"
+                formType="text"
+                name={field.name}
+              >
+                ユーザー名
+              </Input>
+            )}
+          />
+          {errors.username && <p className="text-red-500 w-[clamp(93px,68vw,400px)]">{errors.username.message}</p>}
         </div>
         <div className="mb-[clamp(10px,2.6vh,40px)]">
-          <Input handleValueChange={handleValueChange} textColor="text-primary" borderColor="border-primary" formType="text" formName="email">メールアドレス</Input>
+          <Controller
+            control={control}
+            rules={{
+              required: "メールアドレスを入力してください", pattern: {
+                value: /^[a-zA-Z0-9.!#$%&'*+/\\=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+                message: "メールアドレスはメールアドレス形式で入力してください",
+              },
+            }}
+            name="email"
+            render={({ field }) => (
+              <Input
+                value={field.value}
+                onChangeText={field.onChange}
+                onBlur={field.onBlur}
+                textColor="text-primary"
+                borderColor="border-primary"
+                formType="text"
+                name={field.name}
+              >
+                メールアドレス
+              </Input>
+            )}
+          />
+          {errors.email && <p className="text-red-500 w-[clamp(93px,68vw,400px)]">{errors.email.message}</p>}
         </div>
+
         <div className="mb-[clamp(10px,2.6vh,40px)]">
-          <PasswordInput handleValueChange={handleValueChange} textColor="text-primary" borderColor="border-primary" formName="password">パスワード</PasswordInput>
+          <Controller
+            control={control}
+            rules={{
+              required: "パスワードを入力してください",
+              minLength: {
+                value: 10,
+                message: "パスワードは10桁以上で入力してください",
+              },
+              pattern: {
+                value: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_])[A-Za-z0-9\W_]+$/,
+                message: "パスワードは英大文字・小文字・数字・記号を各1文字以上使用してください",
+              }
+            }}
+            name="password"
+            render={({ field }) => (
+              <PasswordInput
+                value={field.value}
+                onChangeText={field.onChange}
+                onBlur={field.onBlur}
+                textColor="text-primary"
+                borderColor="border-primary"
+                formType="password"
+                name={field.name}
+              >
+                パスワード
+              </PasswordInput>
+            )}
+          />
+          {errors.password && <p className="text-red-500 w-[clamp(93px,68vw,400px)]">{errors.password.message}</p>}
         </div>
+
         <div className="mb-[clamp(28px,6.6vh,112px)]">
-          <PasswordInput handleValueChange={handleValueChange} textColor="text-primary" borderColor="border-primary" formName="confirmPassword">確認パスワード</PasswordInput>
+          <Controller
+            control={control}
+            rules={{
+              required: "確認パスワードを入力してください",
+              validate: {
+                validate: (value) =>
+                  value === getValues("password") ||
+                  "パスワードと確認パスワードが異なります",
+              }
+            }}
+            name="confirmPassword"
+            render={({ field }) => (
+              <PasswordInput
+                value={field.value}
+                onChangeText={field.onChange}
+                onBlur={field.onBlur}
+                textColor="text-primary"
+                borderColor="border-primary"
+                formType="password"
+                name={field.name}
+              >
+                確認パスワード
+              </PasswordInput>
+            )}
+          />
+          {errors.confirmPassword && <p className="text-red-500 w-[clamp(93px,68vw,400px)]">{errors.confirmPassword.message}</p>}
         </div>
+
         <div className="mb-24.5">
-          <TwoButton buttonTitle1="確認画面へ" buttonTitle2="戻る" buttonBgColor="bg-primary" buttonTextColor="text-secondary" onPrimaryClick={onPrimaryClick} onSecondaryClick={onSecondaryClick} />
+          <TwoButton buttonTitle1="確認画面へ" buttonTitle2="戻る" buttonBgColor="bg-primary" buttonTextColor="text-secondary" onPrimaryClick={handleSubmit(onPrimaryClick)} onSecondaryClick={onSecondaryClick} />
         </div>
       </div>
     </div>
