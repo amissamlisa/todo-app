903672c8c759f7313f679e2b60bb95f70ecfd51d
misaki <mwtj96762@gmail.com>
2025-12-30 18:03:09 +0900

    refactor: InputProps を react-hook-form 対応の props 構成に変更

--- frontend/src/shared/types/input.ts
+++ frontend/src/shared/types/input.ts
@@ -1,14 +1,15 @@
 import { type ReactNode } from "react";
 type FormTextColor = "text-primary" | "text-secondary";
 type FormBorderColor = "border-primary" | "border-secondary";
-type HandleValueChange = (formName: string, newValue: string) => void;
 type InputType = "text" | "password";
 
 export interface InputProps {
-  borderColor: FormBorderColor
-  textColor: FormTextColor
-  formName: string
-  formType?: InputType
-  handleValueChange: HandleValueChange
-  children: ReactNode
+  value?: string;
+  onChangeText?: (value: string) => void;
+  onBlur?: () => void;
+  borderColor: FormBorderColor;
+  textColor: FormTextColor;
+  name: string;
+  formType?: InputType;
+  children: ReactNode;
 }
