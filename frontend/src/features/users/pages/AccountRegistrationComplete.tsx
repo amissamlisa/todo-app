05607a5f71dbfec568f2c0e43d88e5694b667940
misaki <mwtj96762@gmail.com>
2025-12-31 18:21:14 +0900

    feat: #31 会員登録完了画面の実装

---
+++ frontend/src/features/users/pages/AccountRegistrationComplete.tsx
@@ -0,0 +1,26 @@
+import { memo } from "react";
+import { Header } from "../../../shared/components/molecules/Header";
+import rainbowImg from "../../../assets/rainbow_cloud.png";
+import { Button } from "../../../shared/components/atoms/Button";
+import { useNavigate } from 'react-router-dom';
+
+
+export const AccountRegistrationComplete = memo(() => {
+  const navigate = useNavigate();
+  const onButtonClick = () => {
+    navigate("/", {replace: true});
+  }
+
+  return (
+    <div className="overflow-y-auto h-screen ">
+      <Header />
+      <div className="flex flex-col justify-center items-center mt-[clamp(68.5px,16.2vh,274px)]">
+        <h2 className="text-primary">アカウント登録完了しました</h2>
+        <img className=" w-[clamp(115px,59.2vw,462px)] " src={rainbowImg} />
+        <div className="mb-[clamp(251px,59.5vh,1006px)]">
+        <Button onButtonClick={onButtonClick} buttonColor="bg-primary" textColor="text-secondary">ログイン画面へ</Button>
+        </div>
+      </div>
+    </div>
+  )
+})
