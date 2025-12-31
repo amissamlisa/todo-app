1473b02418f6a11cdaa55ed9658f6cd24a0d565d
misaki <mwtj96762@gmail.com>
2025-12-31 18:19:58 +0900

    feat: #30 新規会員登録確認画面の実装

---
+++ frontend/src/features/users/pages/AccountRegistrationConfirm.tsx
@@ -0,0 +1,40 @@
+import { memo } from "react";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Header } from "../../../shared/components/molecules/Header";
+import { TwoButton } from "../../../shared/components/molecules/TwoButton";
+import { RegistrationConfirmForm } from "../../../shared/components/molecules/RegistrationConfirmForm";
+import type { RegistrationFormType } from "../type/registrationForm";
+
+
+
+export const AccountRegistrationConfirm = memo(() => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const data = location.state as RegistrationFormType;
+  const userData = [
+    {title :"ユーザー名", value: data.username},
+    {title: "メールアドレス", value: data.email},
+    {title: "パスワード", value: data.password}
+  ]
+  
+  const onPrimaryClick = () => {
+    navigate("/user-registration/complete", {replace: true});
+  };
+  const onSecondaryClick = () => {
+    navigate("/user-registration", {replace: true});
+  }
+  return (
+    <div className="overflow-y-auto h-screen ">
+      <Header />
+      <div className="bg-secondary flex flex-col items-center">
+        <h1 className="text-primary mt-[clamp(15px,4vh,60px)] mb-[clamp(20px,4.8vh,80px)] text-2xl">新規会員登録確認</h1>
+        <div className="mt-[clamp(15px,4vh,60px) mb-[clamp(10px,2.6vh,40px)]">
+          <RegistrationConfirmForm titleColor="text-primary" subTitleColor="text-secondary" backgroundColor="bg-primary" data={userData}>会員情報</RegistrationConfirmForm>
+        </div>
+        <div className="mt-[clamp(76.5px,18.1vh,306px)] mb-24.5">
+          <TwoButton buttonTitle1="登録" buttonTitle2="戻る" buttonBgColor="bg-primary" buttonTextColor="text-secondary" onPrimaryClick={onPrimaryClick} onSecondaryClick={onSecondaryClick} />
+        </div>
+      </div>
+    </div>
+  )
+})
