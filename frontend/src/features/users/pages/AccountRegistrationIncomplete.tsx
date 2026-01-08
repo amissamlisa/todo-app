08e26dc1b95cbac2aeee45b1eb3b5dc551d2b443
misaki <mwtj96762@gmail.com>
2026-01-08 23:47:07 +0900

    style: #32 アカウント登録失敗画面にタイトルを追加

--- frontend/src/features/users/pages/AccountRegistrationIncomplete.tsx
+++ frontend/src/features/users/pages/AccountRegistrationIncomplete.tsx
@@ -10,17 +10,18 @@ export const AccountRegistrationIncomplete = memo(() => {
   const location = useLocation();
   const errMessage = location.state.error;
   const onButtonClick = () => {
-    navigate("/", {replace: true});
+    navigate("/", { replace: true });
   }
 
   return (
     <div className="overflow-y-auto h-screen ">
       <Header />
       <div className="flex flex-col justify-center items-center mt-[clamp(68.5px,16.2vh,274px)]">
+        <h2 className="text-primary mt-[clamp(15px,9.1vh,60px)] mb-[clamp(20px,4.7vh,80px)] text-2xl">アカウント登録失敗</h2>
         <h2 className="text-primary">{errMessage}</h2>
         <img className=" w-[clamp(115px,59.2vw,462px)] " src={rainCloudImg} />
-        <div className="mb-[clamp(251px,59.5vh,1006px)]">  
-        <Button onButtonClick={onButtonClick} buttonColor="bg-primary" textColor="text-secondary">ログイン画面へ</Button>
+        <div className="mb-[clamp(251px,59.5vh,1006px)]">
+          <Button onButtonClick={onButtonClick} buttonColor="bg-primary" textColor="text-secondary">ログイン画面へ</Button>
         </div>
       </div>
     </div>
