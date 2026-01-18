ba139a86c1af7c78fb944ae58c6414294e401476
misaki <mwtj96762@gmail.com>
2026-01-18 20:40:00 +0900

    feat: #33 パスワード再設定完了画面の実装

---
+++ frontend/src/features/users/pages/PasswordResetComplete.tsx
@@ -0,0 +1,25 @@
+import { memo } from "react";
+import { Header } from "../../../shared/components/molecules/Header";
+import rainbowImg from "../../../assets/rainbow_cloud.png";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../../shared/components/atoms/Button";
+
+export const PasswordResetComplete = memo(() => {
+  const navigate = useNavigate();
+  const onButtonClick = () => {
+    navigate("/", { replace: true });
+  }
+  return (
+    <div className="overflow-y-auto h-screen ">
+      <Header />
+      <div className="flex flex-col justify-center items-center mt-[clamp(37.5px,9.1vh,154px)]">
+        <h2 className="text-primary mb-[clamp(27.5px,6.5vh,110px)] text-2xl">パスワード再設定完了</h2>
+        <h2 className="text-primary w-[clamp(112.5px,57.6vw,450px)]">パスワード再設定が完了しました。ログイン時は新しいパスワードを使用してください</h2>
+          <img className=" w-[clamp(115px,59.2vw,462px)] " src={rainbowImg} />
+        <div className="mb-[clamp(251px,59.5vh,1006px)]">
+          <Button onButtonClick={onButtonClick} buttonColor="bg-primary" textColor="text-secondary">ログイン画面へ</Button>
+        </div>
+      </div>
+    </div>
+  )
+})
