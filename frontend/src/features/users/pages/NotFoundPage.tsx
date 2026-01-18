28bab0f98668896d1f4a6e82ff9422cf62268699
misaki <mwtj96762@gmail.com>
2026-01-18 20:41:50 +0900

    feat: 存在しないページのエラー画面実装

---
+++ frontend/src/features/users/pages/NotFoundPage.tsx
@@ -0,0 +1,26 @@
+import { memo } from "react";
+import { Header } from "../../../shared/components/molecules/Header";
+import notFoundImg from "../../../assets/rain-cloud.png";
+import { Button } from "../../../shared/components/atoms/Button";
+import { useNavigate } from "react-router-dom";
+
+export const NotFoundPage = memo(() => {
+  const navigate = useNavigate();
+  const onButtonClick = () => {
+    navigate("/", { replace: true });
+  }
+  return (
+    <div className="overflow-y-auto h-screen ">
+      <Header />
+      <div className="flex flex-col justify-center items-center mt-[clamp(37.5px,9.1vh,154px)]">
+        <h2 className="text-primary mb-[clamp(27.5px,6.5vh,110px)] text-2xl">ページが見つかりません</h2>
+        <h2 className="text-primary w-[clamp(112.5px,57.6vw,450px)]">申し訳ございません。お探しのページは存在しないか、移動した可能性があります</h2>
+        <img className=" w-[clamp(115px,59.2vw,462px)] " src={notFoundImg} />
+        <div className="mb-[clamp(251px,59.5vh,1006px)]">
+          <Button onButtonClick={onButtonClick} buttonColor="bg-primary" textColor="text-secondary">ログイン画面へ</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+)
