be1653c9aade001f457c442b1319a1b90cf840b9
misaki <mwtj96762@gmail.com>
2026-01-19 23:04:57 +0900

    feat: #34 ログアウト画面の実装

---
+++ frontend/src/features/users/pages/Logout.tsx
@@ -0,0 +1,26 @@
+import { memo } from "react";
+import { Header } from "../../../shared/components/molecules/Header";
+import { Button } from "../../../shared/components/atoms/Button";
+import { useNavigate } from 'react-router-dom';
+import logoIcon from "../../../assets/cloud_icon.png"
+
+export const Logout = memo(() => {
+  const navigate = useNavigate();
+  const onButtonClick = () => {
+    navigate("/", { replace: true });
+  }
+
+  return (
+    <div className="overflow-y-auto h-screen ">
+      <Header />
+      <div className="flex flex-col justify-center items-center mt-[clamp(37.5px,9.1vh,154px)]">
+        <h2 className="text-primary mb-[clamp(27.5px,6.5vh,0px)] text-2xl">ログアウト</h2>
+        <h2 className="text-primary w-[clamp(112.5px,57.6vw,450px)]">ログアウトしました。あなたが育てた雲は、またここから成長を始めます。</h2>
+        <img className=" w-[clamp(69.5px,35.6vw,278px)] " src={logoIcon} />
+        <div className="mb-[clamp(251px,59.5vh,1006px)]">
+          <Button onButtonClick={onButtonClick} buttonColor="bg-primary" textColor="text-secondary">ログイン画面へ</Button>
+        </div>
+      </div>
+    </div>
+  )
+})
