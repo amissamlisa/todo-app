453b7df63f1357a0ef1004a3693a80bc8cc47f2e
misaki <mwtj96762@gmail.com>
2026-01-25 01:29:43 +0900

    style:

--- frontend/src/features/users/pages/AccountRegistrationIncomplete.tsx
+++ frontend/src/features/users/pages/AccountRegistrationIncomplete.tsx
@@ -16,7 +16,7 @@ export const AccountRegistrationIncomplete = memo(() => {
   return (
     <div className="overflow-y-auto h-screen ">
       <Header />
-      <div className="flex flex-col justify-center items-center mt-[clamp(68.5px,16.2vh,274px)]">
+      <div className="flex flex-col justify-center items-center">
         <h2 className="text-primary mt-[clamp(15px,9.1vh,60px)] mb-[clamp(20px,4.7vh,80px)] text-2xl">アカウント登録失敗</h2>
         <h2 className="text-primary">{errMessage}</h2>
         <img className=" w-[clamp(115px,59.2vw,462px)] " src={rainCloudImg} />
