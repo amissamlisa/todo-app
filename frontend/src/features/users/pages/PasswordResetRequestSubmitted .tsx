191f13335fb0f824a7479083d1d664055006511d
misaki <mwtj96762@gmail.com>
2026-01-27 22:55:35 +0900

    refactor: #33 パスワードリセット完了画面の名称とメッセージを修正
    - ファイル名を変更: PasswordResetMessageSent -> PasswordResetRequestSubmitted
    - メッセージを「パスワード再設定用のメールを送信しました。」から「入力されているメールアドレスが登録されている場合、パスワード再設定用のメールが送信されます。」の表現に変更

--- frontend/src/features/users/pages/PasswordResetRequestSubmitted .tsx
+++ frontend/src/features/users/pages/PasswordResetRequestSubmitted .tsx
@@ -27,8 +27,9 @@ export const PasswordResetMessageSent = memo(() => {
     <div className="overflow-y-auto h-screen ">
       <Header />
       <div className="flex flex-col justify-center items-center mt-[clamp(37.5px,9.1vh,154px)]">
-        <h2 className="text-primary mb-[clamp(27.5px,6.5vh,0px)] text-2xl">メールをご確認ください</h2>
-        <h2 className="text-primary w-[clamp(112.5px,57.6vw,450px)]">パスワード再設定用のメールを送信しました。メールに記載されているリンクから再設定をおこなってください </h2>
+        <h2 className="text-primary mb-[clamp(9.5px,2.3vh,83px)] text-2xl">メールをご確認ください</h2>
+        <h2 className="text-primary w-[clamp(134.5px,68.7vw,536px)]">入力されているメールアドレスが登録されている場合、パスワード再設定用のメールが送信されます。
+        その場合メールに記載されているリンクから再設定をおこなってください。</h2>
         <img className=" w-[clamp(115px,59.2vw,462px)] " src={rainbowImg} />
         <div className="mb-[clamp(251px,59.5vh,1006px)]">
           <Button onButtonClick={onButtonClick} buttonColor="bg-primary" textColor="text-secondary">ログイン画面へ</Button>
