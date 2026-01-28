21de64f3abf6f5b86c637aa2ca2f5f58a28e7253
misaki <mwtj96762@gmail.com>
2026-01-28 23:13:11 +0900

    fix: バリデーションエラーのメッセージ取得を辞書キーアクセスに修正

--- backend/utils/parse.py
+++ backend/utils/parse.py
@@ -4,7 +4,7 @@ def parse_validation_errors(errors: List[Dict]):
   parsed_error = {}
   for err in errors:
     value = " ".join(map(str, err["loc"][1:]))
-    message = err.msg
+    message = err["msg"]
     parsed_error[value] = message
   
   return parsed_error
