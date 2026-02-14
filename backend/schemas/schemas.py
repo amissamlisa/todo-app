fb77785a3f94b72d8e45232aeb50bec11e4c586c
misaki <mwtj96762@gmail.com>
2026-02-15 00:27:17 +0900

    fix: GoalsRequestの項目修正とバリデーション変更
    - total_estimated_timeを削除
    - 開始日と終了日のバリデーションをmodel.pyに合わせる
    - クオーテーションの統一
    - task_creation_ruleのdefault値を設定
    - goal_nameの文字数を50 → 100に変更

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -14,29 +14,30 @@ class GoalsRequest(BaseModel):
     status: str = GoalsStatusEnum.Unachieved.value
     weekday_available_time: int = Field(ge=1, le=720)
     weekends_available_time: int = Field(ge=1, le=720)
-    total_estimated_time: int = Field(ge=1)
-    task_creation_rule: Optional[str] = Field(max_length=800)
+    task_creation_rule: Optional[str] = Field(default=None, max_length=800)
 
     @model_validator(mode="after")
     def check_dates(self):
-        if self.start_day >= self.target_day:
+        if self.start_day > self.target_day:
             raise ValueError("target_dayはstart_dayよりも後でなければならない")
         return self
 
 
-
 class GoalsTasksOut(BaseModel):
-    goal_task_name: str = Field(max_length=50)
+    goal_task_name: str = Field(max_length=100)
     deadline: datetime.date
     estimated_time: int = Field(ge=1, le=720)
+
 
 class GoalRequestWithTasks(BaseModel):
     goal: GoalsRequest
     goal_tasks_list: Optional[List[GoalsTasksOut]] = None
 
+
 class SaveRequest(BaseModel):
     goal: GoalsRequest
     goal_tasks: list[GoalsTasksOut]
+
 
 class UserRequest(BaseModel):
     username: str = Field(min_length=1)
@@ -44,22 +45,24 @@ class UserRequest(BaseModel):
     confirmation_password: str = Field(min_length=10)
     email: EmailStr
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def check_password(self):
         pw = self.password
         cpw = self.confirmation_password
 
-        pattern = r'^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_])[A-Za-z0-9\W_]{10,}$'
+        pattern = r"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_])[A-Za-z0-9\W_]{10,}$"
 
         if re.match(pattern, pw) is None:
-            raise ValueError('大・小英数字・記号がそれぞれ1文字ずつ含まれていません')
+            raise ValueError("大・小英数字・記号がそれぞれ1文字ずつ含まれていません")
 
         if pw != cpw:
             raise ValueError("パスワードと確認パスワードが一致しません")
         return self
 
+
 class PasswordResetEmailRequest(BaseModel):
     email: EmailStr
+
 
 class PasswordResetRequest(BaseModel):
     password: str
