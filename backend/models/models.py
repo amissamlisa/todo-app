9adf2098cb8d327b484db385f225308ab92733c2
misaki <mwtj96762@gmail.com>
2026-02-18 00:00:50 +0900

    fix: goal_task_nameの文字数を50から100に変更

--- backend/models/models.py
+++ backend/models/models.py
@@ -96,7 +96,7 @@ class GoalsTasks(Base):
 
     goal_task_id: Mapped[int] = mapped_column(primary_key=True)
     goal_id: Mapped[int] = mapped_column(ForeignKey("goals.goal_id"), nullable=False)
-    goal_task_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    goal_task_name: Mapped[str] = mapped_column(String(100), nullable=False)
     goal_task_status: Mapped[str] = mapped_column(default=GoalsTasksStatusEnum.Todo.value, nullable=False)
     deadline: Mapped[Date] = mapped_column(Date, nullable=False)
     estimated_time: Mapped[int] = mapped_column(Integer, nullable=False)
