4b053860611482605bdc845fd3ef874631d81ec9
misaki <mwtj96762@gmail.com>
2026-03-15 20:38:04 +0900

    feat: カンバンカード用Cards型定義を追加

---
+++ frontend/src/shared/types/cards.tsx
@@ -0,0 +1,9 @@
+export interface Cards {
+  goal_task_id: number;
+  order_num: number;
+  goal_task_status: string;
+  goal_task: string;
+  time: number;
+  deadline: string;
+  onDeleteTask?: (goal_task_id: number) => void;
+}
