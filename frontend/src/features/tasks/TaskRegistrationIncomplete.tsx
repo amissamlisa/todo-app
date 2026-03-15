ad82d1d48159b1d3ed8d21029f19eb52b5907773
misaki <mwtj96762@gmail.com>
2026-03-15 20:24:15 +0900

    refactor: タスク登録失敗画面をIncompleteコンポーネントへ置き換え

--- frontend/src/features/tasks/TaskRegistrationIncomplete.tsx
+++ frontend/src/features/tasks/TaskRegistrationIncomplete.tsx
@@ -1,8 +1,6 @@
 import { memo, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { HeaderWithLogoutIcon } from "../../shared/components/molecules/HeaderWithLogoutIcon";
-import rainCloudImg from "../../assets/rain-cloud.png";
-import { Button } from "../../shared/components/atoms/Button";
+import { Incomplete } from "../../shared/components/pages/Incomplete";
 
 export const TaskRegistrationIncomplete = memo(() => {
   const navigate = useNavigate();
@@ -20,24 +18,15 @@ export const TaskRegistrationIncomplete = memo(() => {
   }
 
   const onButtonClick = () => {
-    navigate("/tasks-registration", { replace: true });
+    navigate("/top", { replace: true });
   };
 
   return (
-    <div className="overflow-y-auto h-screen ">
-      <HeaderWithLogoutIcon />
-      <div className="flex flex-col justify-center items-center">
-        <h2 className="text-primary mt-[clamp(15px,9.1vh,60px)] mb-[clamp(20px,4.7vh,80px)] text-2xl">
-          タスク登録失敗
-        </h2>
-        <h2 className="text-primary">{errMessage}</h2>
-        <img className=" w-[clamp(115px,59.2vw,462px)] " src={rainCloudImg} />
-        <div className="mb-[clamp(251px,59.5vh,1006px)]">
-          <Button onButtonClick={onButtonClick} buttonColor="bg-primary" textColor="text-secondary">
-            タスク登録画面へ
-          </Button>
-        </div>
-      </div>
-    </div>
+    <Incomplete
+      title="タスク登録失敗"
+      message={errMessage}
+      buttonText="タスク登録画面へ"
+      onButtonClick={onButtonClick}
+    />
   );
 });
